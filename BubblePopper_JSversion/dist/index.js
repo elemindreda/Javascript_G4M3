a87309856cdfff47f443ff2f5ab5d26f4ad0147f
elemindreda <59729491+elemindreda@users.noreply.github.com>
2021-04-07 20:55:40 +1000

    updated with enemies

--- BubblePopper_JSversion/dist/index.js
+++ BubblePopper_JSversion/dist/index.js
@@ -68,10 +68,10 @@ class Player{
             ctx.stroke();
         }
         ctx.fillStyle = 'clear';
-        ctx.beginPath();
-        // ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
-        ctx.fill();
-        ctx.closePath();
+        // ctx.beginPath();
+        // // ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+        // ctx.fill();
+        // ctx.closePath();
         // ctx.fillRect(this.x, this.y, this.radius, 10)
 
         //if Fish is moving Left
@@ -102,6 +102,7 @@ const player = new Player();
 //Bubbles
 const bubblesArray = [];
 const bubblesEffects = [];
+const enemiesArray = [];
 const bubbleImage = new Image();
 bubbleImage.src = './Assets/Images/bubble_pop_frame_01.png';
 
@@ -165,6 +166,7 @@ bubblePopSound2.src = './Assets/Sounds/pop3.ogg'
 function spawnBubbles(){
     if (gameFrame % 50 == 0){
         bubblesArray.push(new Bubble());
+        // console.log('bubble spawned')
     }
 
     bubblesEffects.forEach((bubbleseffect, index) => {
@@ -226,20 +228,103 @@ function drawBackground(){
 }
 
 // Enemies
-const enemyImage = new Image();
-enemyImage.src = './Assets/Images/'
+const enemyImageLeft = new Image();
+const enemyImageRight = new Image();
+enemyImageLeft.src = './Assets/Images/enemy_swim_to_left_sheet.png'
+enemyImageRight.src = './Assets/Images/enemy_swim_to_right_sheet.png'
+
+class Enemy {
+    constructor(){
+        this.direction = Math.random() < 0.5 ? 'Left' : 'Right'
+        this.radius = 50
+        this.x = this.direction === 'Left' ? canvas.width + this.radius : 0 - this.radius
+        this.y = Math.random() * canvas.height
+        this.velocity = {
+            x: this.direction === 'Left' ? Math.random()*2 + 3 *-1 : Math.random()*2 + 3,
+            y: Math.random() - 0.5
+        }
+        this.distance = 100
+        this.angle = 0;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.frame = 0;
+        this.spriteWidth = 256;
+        this.spriteHeight = 256;
+    }
+    update(){
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        const dx = this.x - player.x;
+        const dy = this.y - player.y;
+        this.distance = Math.hypot(dx, dy)
+    }
+    draw(){
+        // ctx.fillStyle = 'red';
+        // ctx.beginPath();
+        // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        // ctx.fill();
+        // ctx.closePath();
+        // ctx.stroke();
+        
+
+////////////////////////////////////////////////
+//// Figure out how to do Sprite Animation ////
+//////////////////////////////////////////////
+
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle)
+
+        if (this.direction === 'Left'){
+        ctx.drawImage(enemyImageLeft, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, 
+        this.spriteWidth, this.spriteHeight, 0 - 75, 0 - 70, this.spriteWidth/1.80, this.spriteHeight/1.80)
+        } else{
+            ctx.drawImage(enemyImageRight, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, 
+                this.spriteWidth, this.spriteHeight, 0 - 75, 0 - 70 , this.spriteWidth/1.8, this.spriteHeight/1.8)
+        }
+        ctx.restore();
+    }
+}
+
+function spawnEnemies(){
+    if (gameFrame % 500 == 0 ){
+        enemiesArray.push(new Enemy());
+        console.log(enemiesArray)
+    }
+    enemiesArray.forEach((enemy, index) =>{
+        // console.log(enemy.x)
+        enemy.update();
+        enemy.draw();
+
+        if (enemy.direction == 'Left' && enemy.x < 0 - enemy.radius ){
+            enemiesArray.splice(index, 1)
+        }
+        if (enemy.direction == 'Right' && enemy.x > canvas. width + enemy.radius){
+            enemiesArray.splice(index, 1)
+        }
+        
+    })
+
+    // console.log(enemiesArray)
+    // enemiesArray.forEach((enemy, index) => {
+    //     enemy.update()
+    //     enemy.draw()
+    // })
+}
+
 
 //Animation Loop
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.width);
     drawBackground();
     spawnBubbles();
+    spawnEnemies();
     player.update();
     player.draw();
     ctx.fillStyle = 'black';
     ctx.fillText('score: ' + score, 10, 50);
     gameFrame++;
-    requestAnimationFrame(animate);
+    animation = requestAnimationFrame(animate);
 }
 
 
